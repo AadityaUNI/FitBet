8cd072d0e94b6944db2c8d1a6d2a3532ac1c1612
Aadi566 <aaditya.singh5266@gmail.com>
2025-09-26 22:50:57 +0930

    added betting form

--- frontend/src/pages/BetForm.tsx
+++ frontend/src/pages/BetForm.tsx
@@ -1,11 +1,16 @@
 import SimpleBottomNavigation from "../components/BottomNav/BottomNav";
-
+import { ProfileForm } from "../components/Bets/CreateBet";
 const BettingForm = () => {
   return (
-    <div>
-      <h1>Betting Form</h1>
+    <>
+      <div className="min-h-screen w-full flex flex-col items-center justify-center p-4">
+        <div className="w-full max-w-md mx-auto">
+          <h1 className="text-2xl font-bold text-center mb-6">Start a FitBet</h1>
+          <ProfileForm />
+        </div>
+      </div>
       <SimpleBottomNavigation />
-    </div>
+    </>
   );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +11,10 @@ export default defineConfig({
       interval: 100       // 👈 optional: adjust the polling interval (ms)
     }
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
